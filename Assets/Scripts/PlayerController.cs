b2bffb2535104efabea8b2c8e33ca8eed3dda902
Belf <belfegore.work@gmail.com>
2025-09-22 23:21:08 +0700

    Fix : Canvas to support both android,IOS and window OS , Add : PlayerController Prototype

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Status (For pausing player)")]
+    [SerializeField] private bool isPlaying;
+
+    [Header("Player Configuration")]
+    [SerializeField] private float playerGravity;
+    [SerializeField] private float playerFlyStrength;
+
+    private Vector3 direction;
+
+    void Update()
+    {
+        if (isPlaying && Input.GetMouseButtonDown(0)) direction = Vector3.up * playerFlyStrength;
+
+        if (isPlaying)
+        {
+            direction.y += playerGravity * Time.deltaTime;
+            transform.position += direction * Time.deltaTime;
+        }
+    }
+}
